288a31c9cd8c83572184b4b41e9cddd0ed064c2a
Amitie10g <davidkingnt@gmail.com>
2019-10-26 21:20:14 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ from library import Top500Importer
 # :: Check Python version (3.5 or above)
 if sys.version_info < (3, 5):
     sys.stderr.write("You need python 3.5 or later to run this script\n")
-    sys.exit(2)
+    sys.exit(0)
 
 # :: Check if configuration has been properly set at config.py
 try:
-    # :: Settings
     import config as cfg
 
     wiki_site = cfg.config['site']
@@ -35,11 +34,11 @@ try:
     redis_port = cfg.config['redis_port']
 except (NameError, IndexError) as e:
     sys.stderr.write(str(e) + '\n')
-    sys.exit(2)
+    sys.exit(0)
 
 # :: Get args
 argv = sys.argv[1:]
-usage = 'Usage: python3 main.py [-i <Wikidata item> -t <TOP500 id> | --mass]\n'
+usage = 'Usage: python3 pywikibot/pwb.py main.py [-i <Wikidata item> -t <TOP500 id> | --mass] num\n'
 
 # :: Parse args
 if argv == []:
@@ -67,63 +66,65 @@ if args2 == []:
     sys.exit(0)
 
 try:
-    # :: Call the Top500Importer object
-    top500importer = Top500Importer(wiki_site, wiki_lang, redis_server, redis_port, instance_of, top500url, log_page, status_page)
+    try:
+        # :: Call the Top500Importer object
+        top500importer = Top500Importer(wiki_site, wiki_lang, redis_server, redis_port, instance_of, top500url, log_page, status_page)
 
-    # :: Mass import
-    if args2[0] == 'mass':
-        top500importer.updateStatus(1)
-        top500importer.mass(args2[1])
+        # :: Mass import
+        if args2[0] == 'mass':
+            top500importer.updateStatus(1)
 
+            if top500importer.mass(args2[1]):
+                print('Everything OK\n')
+
+            try:
+                system_status = top500importer.qstat()
+                if system_status.returncode == 0:
+                    sys.exit(128)
+
+                else:
+                    sys.exit(0)
+
+            except FileNotFoundError:
+                sys.exit(0)
+
+        # :: One-file import
+        else:
+            top500importer.updateStatus(1)
+
+            if len(args2) == 2:
+                if top500importer.main(args2[1], args2[0]):
+                    print('Everything OK\n')
+
+                    try:
+                        system_status = top500importer.qstat()
+                        if system_status.returncode == 0:
+                            sys.exit(128)
+
+                        else:
+                            sys.exit(0)
+
+                    except FileNotFoundError:
+                        sys.exit(0)
+
+                else:
+                    sys.exit(2)
+
+            else:
+                print(usage)
+                sys.exit(1)
+
+    except KeyboardInterrupt:
         try:
             system_status = top500importer.qstat()
             if system_status.returncode == 0:
                 sys.exit(128)
-
             else:
                 sys.exit(0)
 
         except FileNotFoundError:
             sys.exit(0)
 
-    # :: One-file import
-    else:
-        top500importer.updateStatus(1)
-
-        if len(args2) == 2:
-            if top500importer.main(args2[1], args2[0]):
-                print('Everything OK\n')
-
-                try:
-                    system_status = top500importer.qstat()
-                    if system_status.returncode == 0:
-                        sys.exit(128)
-
-                    else:
-                        sys.exit(0)
-
-                except FileNotFoundError:
-                    sys.exit(0)
-
-            else:
-                print('Something went wrong\n')
-                sys.exit(2)
-
-        else:
-            print(usage)
-            sys.exit(1)
-
-except KeyboardInterrupt:
-    try:
-        system_status = top500importer.qstat()
-        if system_status.returncode == 0:
-            sys.exit(128)
-
-        else:
-            sys.exit(0)
-
-    except FileNotFoundError:
-        sys.exit(0)
-
 except SystemExit as e:
     top500importer.updateStatus(e)
+	
